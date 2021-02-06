74449ef39c2ce047c00c80491179ad8d4a0e68b3
Aziz <aziz.com>
2021-02-06 01:56:19 +0100

    this is neu commit

--- Excel_Vlan.py
+++ Excel_Vlan.py
@@ -5,13 +5,10 @@ data = pd.read_excel('VLANs.xlsx')
 df = pd.DataFrame(data, columns= ['VLAN','Value'])
 print (df)'''
 import xlrd
- 
-loc = ("VLANs.xlsx")
- 
-wb = xlrd.open_workbook(loc)
+ loc = ("VLANs.xlsx")
+ wb = xlrd.open_workbook(loc)
 sheet = wb.sheet_by_index(0)
- 
-# For row 0 and column 0
+ # For row 0 and column 0
 sheet.cell_value(0, 0)
  
 for i in range(sheet.ncols):
