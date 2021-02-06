3cfac8d25d169512c092a04189db976a572d8ef9
Aziz <aziz.com>
2021-02-06 02:58:41 +0100

    Last Update

--- Excel_Vlan.py
+++ Excel_Vlan.py
@@ -5,9 +5,10 @@ wb = xlrd.open_workbook(loc)
 sheet = wb.sheet_by_index(0)
  # For row 0 and column 0
 sheet.cell_value(0, 0)
-Vlan_new = [2,25,34,24]
+Vlan_new = [2,23,25,24,7,60]
 Vlans_exist = []
 Vlans_Value = []
+Result = []
 #for i in range(sheet.ncols):
  #   print(sheet.cell_value(0, i))
 for i in range(1,sheet.nrows):
@@ -18,3 +19,11 @@ for i in range(1,sheet.nrows):
       Vlans_Value.append(sheet.cell_value(i, 1)) 
 print(Vlans_exist)
 print(Vlans_Value)
+for vlan_Id in Vlan_new:
+    index = 0
+    for ID in Vlans_exist :
+        index +=1
+        if vlan_Id == ID :
+            if Vlans_Value != 1:
+                Result.append(ID)
+print(Result)
