381bfd349e2f3c1cc1698b60db3a07048a189976
Aziz <aziz.com>
2021-02-06 02:16:30 +0100

    neu change

--- Excel_Vlan.py
+++ Excel_Vlan.py
@@ -5,13 +5,14 @@ data = pd.read_excel('VLANs.xlsx')
 df = pd.DataFrame(data, columns= ['VLAN','Value'])
 print (df)'''
 import xlrd
- loc = ("VLANs.xlsx")
- wb = xlrd.open_workbook(loc)
- sheet = wb.sheet_by_index(0)
+
+loc = ("VLANs.xlsx")
+wb = xlrd.open_workbook(loc)
+sheet = wb.sheet_by_index(0)
  # For row 0 and column 0
- sheet.cell_value(0, 0)
+sheet.cell_value(0, 0)
  
- for i in range(sheet.ncols):
+for i in range(sheet.ncols):
     print(sheet.cell_value(0, i))
- for i in range(1,sheet.nrows):
+for i in range(1,sheet.nrows):
     print(int(sheet.cell_value(i, 0)))
